f32f4e6a4796b4754919caddbb4eb0878bbd23e4
volodindmitriy121 <volodindmitriy121@gmail.com>
2017-12-01 20:41:29 +0200

    Add commm, getting files from dir

--- lit/cli.py
+++ lit/cli.py
@@ -1,20 +1,24 @@
 import sys
 import argparse
+import lit.command.InitCommand as incom
+import lit.command.AddCommand as addcom
 
-sys.path.append("AddCommand", "BaseCommand", "CommitCommand", "DiffCommand",
-                "InitCommand", "LogCommand", "RmCommand", "StatusCommand")
+#sys.path.append("AddCommand", "BaseCommand", "CommitCommand", "DiffCommand",
+#                "InitCommand", "LogCommand", "RmCommand", "StatusCommand")
 
-COMMANDS_INITIALIZE = {'add': sys.path.AddCommand('add', 'add files to tracked'),
-                       'commit': sys.path.CommitCommand('commit', 'commit tracked files'),
-                       'diff': sys.path.DiffCommand('diff', 'show difference of two files'),
-                       'init': sys.path.InitCommand('init', 'init lit directory'),
-                       'log': sys.path.LogCommand('log', 'show changes history'),
-                       'rm': sys.path.RmCommand('rm', 'delete selected file'),
-                       'status': sys.path.StatusCommand('status', 'show current status of changes'),
-                       }
+#COMMANDS_INITIALIZE = {'add': sys.path.AddCommand('add', 'add files to tracked'),
+#                       'commit': sys.path.CommitCommand('commit', 'commit tracked files'),
+#                       'diff': sys.path.DiffCommand('diff', 'show difference of two files'),
+#                       'init': sys.path.InitCommand('init', 'init lit directory'),
+#                       'log': sys.path.LogCommand('log', 'show changes history'),
+#                       'rm': sys.path.RmCommand('rm', 'delete selected file'),
+#                       'status': sys.path.StatusCommand('status', 'show current status of changes'),
+#                       }
 
 
-# a = sys.path.InitCommand('init', 'init lit directory')
+init = incom.InitCommand('init', 'init lit directory')
+add = addcom.AddCommand('add', 'add files to tracked')
+
 
 
 def main():
@@ -22,30 +26,31 @@ def main():
     subparsers = parser.add_subparsers()
 
     parser_init = subparsers.add_parser('init', help='init help')
-    parser_init.set_defaults(func=COMMANDS_INITIALIZE['init'].run)
+    #parser_init.set_defaults(func=COMMANDS_INITIALIZE['init'].run)
+    parser_init.set_defaults(func=init.run)
 
     parser_add = subparsers.add_parser('add', help='add help')
-    parser_add.add_argument('path', type=str, help='path help')
-    parser_add.set_defaults(func=COMMANDS_INITIALIZE['add'].run)
+    parser_add.add_argument('path', type=str, help='path help', action='store')
+    parser_add.set_defaults(func=add.run)
 
     parser_rm = subparsers.add_parser('rm', help='rm help')
     parser_rm.add_argument('path', type=str, help='path help')
-    parser_rm.set_defaults(func=COMMANDS_INITIALIZE['rm'].run)
+   # parser_rm.set_defaults(func=COMMANDS_INITIALIZE['rm'].run)
 
     parser_commit = subparsers.add_parser('commit', help='commit help')
     parser_commit.add_argument('message', type=str, help='message help')
-    parser_commit.set_defaults(func=COMMANDS_INITIALIZE['commit'].run)
+    #parser_commit.set_defaults(func=COMMANDS_INITIALIZE['commit'].run)
 
     parser_diff = subparsers.add_parser('diff', help='diff help')
     parser_diff.add_argument('file-1-path', type=str, help='file-1-path help')
     parser_diff.add_argument('file-2-path', type=str, help='file-2-path help')
-    parser_diff.set_default(func=COMMANDS_INITIALIZE['diff'].run)
+    #parser_diff.set_default(func=COMMANDS_INITIALIZE['diff'].run)
 
     parser_log = subparsers.add_parser('log', help='log help')
-    parser_log.add_defaults(func=COMMANDS_INITIALIZE['log'].run)
+    #parser_log.add_defaults(func=COMMANDS_INITIALIZE['log'].run)
 
     parser_status = subparsers.add_parser('status', help='status help')
-    parser_status.add_defaults(func=COMMANDS_INITIALIZE['status'].run)
+    #parser_status.add_defaults(func=COMMANDS_INITIALIZE['status'].run)
 
     result = parser.parse_args(sys.argv[1:])
     print(result)
--- lit/command/AddCommand.py
+++ lit/command/AddCommand.py
@@ -1,12 +1,33 @@
 from lit.command.BaseCommand import BaseCommand
 import os
+import sys
+import json
 
 class AddCommand(BaseCommand):
+
+    path = os.getcwd()
+
     def __init__(self, name, help_message):
         super().__init__(name, help_message)
 
-    def run(self, **args):
+    def run(self, *args):
+        self.get_file_list(sys.argv[2:])
 
-        if not super().run():
-           return False
-        raise NotImplementedError()
+ #       if args in self.get_file_list() and args not in self.tracked_files:
+ #           self.tracked_files.append(args)
+ #          self.save_tracked_files()
+
+
+    def get_file_list(self, *args):
+        print(args[0][0])
+        file_list = os.listdir(args[0][0])
+        return list(filter(lambda file: file[0] != '.', file_list))
+
+    def save_tracked_files(self):
+#       with open(self.path + '/.lit/tracked_files.json', 'w') as outfile:
+#           json.dump(self.tracked_files, outfile)
+        pass
+
+#        if not super().run():
+#           return False
+#        raise NotImplementedError()
