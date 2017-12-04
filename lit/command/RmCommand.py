97081c2a11842b4e45ecdbbe2d0d9f8754f74237
volodindmitriy121 <volodindmitriy121@gmail.com>
2017-12-04 16:44:47 +0200

    WIP rm

--- lit/command/RmCommand.py
+++ lit/command/RmCommand.py
@@ -1,26 +1,41 @@
 from lit.command.BaseCommand import BaseCommand
 import json
 import sys
+import os
+import glob
+
 
 class RmCommand(BaseCommand):
     def __init__(self, name, help_message):
         super().__init__(name, help_message)
 
     def run(self, **args):
-        a = open('.lit/tracked_files.json', 'r')
-        tracked = json.load(a)
-        delete_file = sys.argv[2:]
-        a.close()
+        delete_path = sys.argv[2:][0]
 
-        tracked["files"].pop(delete_file)
+        (filepath, filename) = os.path.split(delete_path)
+        (shortname, extension) = os.path.splitext(delete_path)
 
-        b = open('.lit/tracked_files.json', 'w')
-        json.dump(tracked, b)
-        b.close()
+        print(filename)
 
+        if extension == "":
+            print("No extension")
+            delete_list = os.listdir(delete_path)
+            print(delete_list)
 
+        else:
+            print("i have extention" + extension)
 
+            a = open('.lit/tracked_files.json', 'r')
+            tracked = json.load(a)
+            a.close()
 
+            tracked['files'].remove(filename)
+
+            b = open('.lit/tracked_files.json', 'w')
+            json.dump(tracked, b)
+            b.close()
+
+ #
 
 #        if not super().run():
 #           return False
