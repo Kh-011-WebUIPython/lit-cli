ab0eb5db499f316ebee00e2e16ab3e53dbbdef4c
Pavel Belousov <pavel.belousov@yahoo.com>
2017-12-12 15:06:11 +0200

    Fixed critical errors in LogCommand

--- lit/command/LogCommand.py
+++ lit/command/LogCommand.py
@@ -11,7 +11,6 @@ class LogCommand(BaseCommand):
         super().__init__(name, help_message, arguments)
 
     def run(self, **args):
-        serializer = JSONSerializer(SettingsManager.get_var_value('COMMIT_LOG_PATH'))
         serializer = JSONSerializer(LogSettings.PATH)
         logs = serializer.read_all_items()
         json_commit_print(logs)
@@ -19,10 +18,10 @@ class LogCommand(BaseCommand):
 
 def json_commit_print(json):
     for commit in json["commits"]:
-        s = SettingsManager.get_var_value('LOG_COMMIT') + commit["short_hash"] + ";\n" + \
-            SettingsManager.get_var_value('LOG_COMMIT_MESSAGE') + commit["comment"] + ";\n" + \
-            SettingsManager.get_var_value('LOG_USERNAME') + commit["user"] + ";\n" + \
-            SettingsManager.get_var_value('LOG_DATE') + commit["datetime"][:19] + "\n"
+        s = LogSettings.COMMIT + commit["short_hash"] + ";\n" + \
+            LogSettings.COMMIT_MESSAGE + commit["comment"] + ";\n" + \
+            LogSettings.USERNAME + commit["user"] + ";\n" + \
+            LogSettings.DATE + commit["datetime"][:19] + "\n"
     for commit in json[LogSettings.KEY]:
         short_hash_ = commit[CommitSettings.SHORT_HASH]
         comment = commit[CommitSettings.COMMENT]
