fb7bdc053c6e945f10d4a65260fba908d99e00ee
Giperboloid <kostyamol@gmail.com>
2017-12-19 19:18:29 +0200

    initial commit

---
+++ tests/command/TestStatusCommand.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestLogCommand(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_output_for_one_commit(self):
+        pass
